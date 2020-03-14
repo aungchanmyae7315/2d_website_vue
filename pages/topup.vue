09bdf28dc6d261e228e0f9e34282365064c53174
aungchanmyae7315 <aungchanmyae7315>
2020-03-14 13:54:20 +0630

    topup_bank_type

--- pages/topup.vue
+++ pages/topup.vue
@@ -9,12 +9,52 @@
                 </el-page-header>
                  <!-- </nuxt-link> -->
             </el-header>
-            <p>{{$t('After bank transferring is finished, please enter your amount and upload your bank slip below.')}}</p>
+            <!-- <p>{{$t('After bank transferring is finished, please enter your amount and upload your bank slip below.')}}</p> -->
               <!-- <h5>{{$t('Upload Bank transferred Slip')}}</h5> -->
-            
+               <p>၁. သင်၏ Sein Lucky အကောင့်သို့ ဖြည့်လိုသော ငွေပမာဏကို အောက်ပါ Sein Lucky ဘဏ်အကောင့် တစ်ခုထဲသို့ငွေအရင်လွှဲပါ။ ငွေလွှဲမှတ်တမ်းကို ဓာတ်ပုံ (သို့) စခရင်ရှော့ (Screenshot) ရိုက်ထားပါ။</p>
+        <div id="bank_copy_list">
+            <ul class="see_more_list" >
+                <li  v-if="b < limit_by" v-for="(bank_info, b) in bank_account" :key="b">
+                    <div class="demo-type wallet_type">
+                            
+                              <el-image :src="bank_info.bank_icon">
+                                <div slot="placeholder" class="image-slot">
+                                  Loading<span class="dot">...</span>
+                                </div>
+                              </el-image>
+                              <div  class="avatar_text">
+                                <ul style="padding-top:18px;">
+                                  <li style="font-size:10px;">{{bank_info.bank_name}}</li>
+                                  <li style="font-size:10px;" class="bank_number">
+                                    <span class="code text-red">{{ bank_info.card_number }}</span>
+                                    <input type="hidden" readonly :id="'code'+(bank_info.id)" :value="bank_info.card_number">
+                                  </li>
+                                </ul>
+                                </div>
+                                <div class="result_icon">
+                                    <span class="  copy-btn ml-auto" @click.stop.prevent="copyTestingCode(bank_info.id)">
+                                    {{ $t('Copy') }}
+                                  </span>
+                                  </div>
+                              </div>
+                            
+                </li>
+            </ul>
+            <div class="show_more">
+                <a href="javascript:void(0)" 
+                @click="simple_toggle(default_limit, bank_account.length)">{{ limit_by===2?'Show more': 'Hide more'}}
+              </a>
+            </div>
+          
+        </div>
+        <p>၂. ဘဏ်မှငွေလွှဲပြီးနောက် ငွေလွှဲမှတ်တမ်းဓာတ်ပုံ/ screenshot ကိုတင်ပါ။</p>
+
+
+
+
             <div class="avatar-upload">
                     <div class="avatar-edit" v-if="!url">
-                        <label for="imageUpload" class="upload_icon_top"> <i slot="default" class="el-icon-plus"></i></label>
+                        <label for="imageUpload" class="upload_icon_top"> <i slot="default" class="el-icon-plus"></i><span style="padding-left:15px;position:relative;bottom:6px">ငွေလွှဲမှတ်တမ်း ပုံတင်မည်။</span></label>
                         
                         <input type='file' @change="onFileChange" id="imageUpload" accept=".png, .jpg, .jpeg" />
                         <div slot="tip" class="el-upload__tip"></div>
@@ -31,7 +71,7 @@
                 
             </div>
 
-
+            <p>၃. လွှဲခဲ့သော ငွေပမာဏကို ဖြည့်ပါ။</p>
             <el-form   :model="ruleForm" ref="ruleForm"  class="demo-ruleForm" >
                  <el-form-item 
                   prop="tran_amount"
@@ -39,13 +79,17 @@
                 { required: true, message: $t('amount_required')},
                 
               ]"
-                 :label="$t('Enter transferred amount')" class="tran_input" >
+                  class="tran_input" >
                     <el-input   type="number" :placeholder="$t('Enter transferred amount')" v-model="ruleForm.tran_amount"></el-input>
                 </el-form-item>
             </el-form>
-          
-           
-              <el-button round @click="slip_upload('ruleForm')"  :disabled='submitted'>{{$t('Submit')}}</el-button>
+
+            <p>ငွေမဖြည့်တတ်ပါက ဆက်သွယ်ရန် ဖုန်းများ</p>
+                    <ul class="topup_contact" v-for="(phone , p) in contact" :key="p">
+                        <li><el-button size="medium" type="info"  @click="dial(phone)"   icon="el-icon-phone" >{{phone}}</el-button></li>
+
+                    </ul>
+            <el-button round @click="slip_upload('ruleForm')"  :disabled='submitted'>{{$t('Submit')}}</el-button>
         </div>
        
     </section>
@@ -53,6 +97,7 @@
 </template>
 
 <style>
+
     .topup {
         padding:0 20px;
     }
@@ -91,24 +136,25 @@
     }
     .topup .el-icon-plus:before {
         content: "\e6d9";
-        font-size: 31px;
-        color: #158220;
+        font-size: 27px;
+        color: #fff;
+       
         font-weight: bold;
     }
     .topup .el-button.is-round{
         
         position: relative;
-        margin-top:140px;
+        margin-top:10px;
         max-width: 480px;
         width:100%;
         margin-bottom:20px;
         background-color: #158220;
         color:#fff;
     }
-     .topup .avatar-upload {
+.topup .avatar-upload {
   position: relative;
   
-  margin: 50px auto;
+  margin: 30px auto;
 }
  .topup .avatar-upload .avatar-edit {
   margin:0 auto;
@@ -146,23 +192,82 @@
   background-position: center;
 }
 .topup .upload_icon_top {
-  position: relative;
-    right: 0;
-    padding:50px;
+
     width: 100%;
-    
-    left: 0;
     z-index: 88;
-   
-    bottom: 0;
-    background-color: hsla(120,100%,75%,0.3);
-     box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.12);
-    /* width: 35px; */
-    /* height: 35px; */
-    padding: 26px;
+    color:#fff;
+    padding:6px;
+    border-radius: 21px;
+    background-color: #FFBA15;
+    -webkit-box-shadow: 3px 9px 28px -7px rgba(0,0,0,0.75);
+  -moz-box-shadow: 3px 9px 28px -7px rgba(0,0,0,0.75);
+  box-shadow: 3px 9px 28px -7px rgba(0,0,0,0.75);
     
 }
+.wallet_type .el-avatar--circle {
+      border-radius: 22%;
+    }
+    .wallet_type .el-button {
+      font-size: 11px;
+    }
+    .wallet_type .el-avatar ul {
+      padding:5px;
+    }
+    .wallet_type .el-avatar ul li {
+      color:#000;
+    }
+    .wallet_type img {
+      width:50px;
+      height:auto;
+      float:left;
+      border-radius: 9px;
+      /* margin-right:8px; */
+    }
+    .topup .avatar_text {
+      color:#000;
+    }
+    .see_more_list {
+      list-style: none;
+      padding:20px;
+     
+      margin:0;
     
+    }
+    .see_more_list img {
+      -webkit-box-shadow: 0px 5px 23px -7px rgba(0,0,0,0.75);
+      -moz-box-shadow: 0px 5px 23px -7px rgba(0,0,0,0.75);
+      box-shadow: 0px 5px 23px -7px rgba(0,0,0,0.75);
+    }
+    #bank_copy_list {
+      margin-bottom:30px;
+       border-radius: 9px;
+      -webkit-box-shadow: 3px 6px 28px -7px rgba(0,0,0,0.75);
+      -moz-box-shadow: 3px 6px 28px -7px rgba(0,0,0,0.75);
+      box-shadow: 3px 6px 28px -7px rgba(0,0,0,0.75);
+    }
+      .see_more_list .copy-btn {
+      cursor: pointer;
+      color:#fff;
+      background:#158220;
+      padding:4px 7px;
+      border-radius: 23px;
+      font-size:11px;
+    }
+    .show_more {
+      text-align: center;
+      padding: 8px;
+      border-top: 1px solid #b8b8b8
+    }
+    .topup_contact {
+      margin:0;
+      padding:0;
+      text-align: center;
+      list-style: none;
+    }
+    .topup_contact li {
+      margin:20px auto;
+    }
+
 </style>
 
 <script>
@@ -175,7 +280,17 @@ export default {
     })
     },
     data() {
-        return {
+        return { 
+          
+          default_limit: 2,
+          limit_by: 2,
+          contact:[],
+          bank_account:'',
+          card_number:'',
+          visible: false,
+          profile:'',
+          message:'',
+           display: false,
           submitted:false,
             ruleForm: {
                   tran_amount:'',
@@ -191,10 +306,69 @@ export default {
             // file:'',
         }
     },
+     created() {
+      
+          this.$axios.get('/v1/admin-bank')
+              .then(response => {
+                this.bank_account = response.data.data
+              })  
+          let token = localStorage.getItem('token');
+        if(token) {
+              this.$axios.get("/v1/profile",
+                    {headers: {
+                               "Authorization": "Bearer "+token
+                         }
+                        })
+                    .then(response => {
+                     this.profile = response.data.data
+
+                })
+        }
+         this.$axios.get("/v1/contact")
+    
+            
+                .then(response => {
+                    this.contact = response.data.data[0].phone_number
+            });
+                
+    },
     methods: {
+         simple_toggle(default_limit, filters_length) {
+            this.limit_by = (this.limit_by === default_limit) ? filters_length : default_limit;
+        },
+        dial: function(data){
+          window.location ='tel:'+data;
+        },
+       copyTestingCode (data) {
+        
+          let testingCodeToCopy = document.querySelector('#code'+data)
+          testingCodeToCopy.setAttribute('type', 'text')    
+           testingCodeToCopy.setAttribute('readonly', true);
+          testingCodeToCopy.select()
+
+          try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+             this.$message({
+               message:this.$t('copy_success'),
+              position: 'top',
+              showClose: true,
+          
+            });
+            
+          } catch (err) {
+            alert('Oops, unable to copy');
+          }
+          testingCodeToCopy.setAttribute('type', 'hidden')
+          window.getSelection().removeAllRanges()
+        },
+       
           goBack() {
              this.$router.push(`wallet?lang=${this.$store.state.locale}`); 
          },
+          toggleDropdown() {
+            this.display = !this.display
+          },
          onFileChange(e) {
             var files = e.target.files || e.dataTransfer.files;
             if (!files.length)
