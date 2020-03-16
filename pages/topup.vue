4842e13deaefb01ce7ad7fc2d127c800ee91753e
aungchanmyae7315 <aungchanmyae7315>
2020-03-16 15:24:28 +0630

    topup_camare

--- pages/topup.vue
+++ pages/topup.vue
@@ -57,7 +57,7 @@
                     <div class="avatar-edit" v-if="!url">
                         <label for="imageUpload" class="upload_icon_top"> <i slot="default" class="el-icon-plus"></i><span style="padding-left:15px;position:relative;bottom:6px">ငွေလွှဲမှတ်တမ်း ပုံတင်မည်။</span></label>
                         
-                        <input type='file' @change="onFileChange" id="imageUpload" accept=".png, .jpg, .jpeg" />
+                        <input type='file' accept="image/*" @change="onFileChange" id="imageUpload"  />
                         <div slot="tip" class="el-upload__tip"></div>
                     </div>
             
