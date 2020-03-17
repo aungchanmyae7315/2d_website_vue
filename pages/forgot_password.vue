2f741f711329d6eb6111fb0ac44c85658d3b2dea
aungchanmyae7315 <aungchanmyae7315>
2020-03-17 10:00:03 +0630

    forgotPassword

--- pages/forgot_password.vue
+++ pages/forgot_password.vue
@@ -9,14 +9,12 @@
       <section class="forgot_password_item">
            <p>စကားဝှက်မေ့သွားပါက အောက်ပါ ဝန်ဆောင်မှု ဖုန်းနံပါတ်တစ်ခုသို့ ဆက်သွယ်မေးမြန်းနိုင်ပါသည်။</p>
            <p>အကောင့်ပိုင်ဆိုင်‌ကြောင်း အထောက်အထား မေးခွန်းများ ဖြေဆိုရပါမည်။</p><br>
-           <el-card shadow="always">
-                <div style="float:left;margin-top:6px;"><i class="el-icon-mobile-phone" style="color:#5AAF46;padding:0 10px;font-size:21;font-weight:bold;"></i>09401778547</div>
-                 <a href="tel:09401778547"><el-button round style="float:right;margin:0 auto 20px auto;background:#F0F0FF;color:#5AAF46">Call Now</el-button></a>
+          
+           <el-card shadow="always" v-for="(phone , p) in contact" :key="p">
+                <div style="float:left;margin-top:6px;"><i class="el-icon-mobile-phone" style="color:#5AAF46;padding:0 10px;font-size:21;font-weight:bold;"></i>{{phone}}</div>
+                <el-button round @click="dial(phone)"  style="float:right;margin:0 auto 20px auto;background:#F0F0FF;color:#5AAF46">Call Now</el-button>
             </el-card>
-            <el-card shadow="always">
-                <div style="float:left;margin-top:6px;"><i class="el-icon-mobile-phone" style="color:#5AAF46;padding:0 10px;font-size:21;font-weight:bold;"></i>09889973993</div>
-                 <a href="tel:09889973993"><el-button round style="float:right;margin:0 auto 20px auto;background:#F0F0FF;color:#5AAF46">Call Now</el-button></a>
-            </el-card>
+           
             <!-- <el-card shadow="always">
                 <div style="float:left;margin-top:6px;"><i class="el-icon-mobile-phone" style="color:#5AAF46;padding:0 10px;font-size:21;font-weight:bold;"></i>09774134567</div>
                  <a href="tel:123-456-7890"><el-button round style="float:right;margin:0 auto 20px auto;background:#F0F0FF;color:#5AAF46">Call Now</el-button></a>
@@ -63,16 +61,25 @@ export default {
     },
     data() {
         return {
-         
+           contact:[],
         }
     },
      methods: {
         goBack() {
              this.$router.push(`/login?lang=${this.$store.state.locale}`); 
          },
+        dial: function(data){
+            window.location ='tel:'+data;
+        },
      },
      created() {
-         
-     }
+       this.$axios.get("/v1/contact")
+    
+            
+                .then(response => {
+                    this.contact = response.data.data[0].phone_number
+            });
+     },
+
 }
 </script>
--- pages/login.vue
+++ pages/login.vue
@@ -73,7 +73,7 @@
           <el-button type="success" round @click="submitForm('ruleForm')">{{$t('login_title')}}</el-button>
 
                 <!-- <el-button @click="resetForm('numberValidateForm')">Reset</el-button> -->
-                          <nuxt-link :to="`${$t('/Forgotpassword')}?lang=${$store.state.locale}`">
+                          <nuxt-link :to="`${$t('/forgot_password')}?lang=${$store.state.locale}`">
                 <el-button type="text"  style="color:#158220">{{$t('forgot_password')}}</el-button>
                           </nuxt-link>
             </el-form-item>
